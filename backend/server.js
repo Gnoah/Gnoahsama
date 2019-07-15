314c924aabe7d05bd0d7410cca0072b0cc4ed118
gnoah <ralahajanaharynoah@gmail.com>
2019-07-15 16:18:22 +0300

    test1

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const config = require('./config/dbconfig');
+const methodOverride = require('method-override');
+var cors = require('cors');
+const app = express();
+require('./models/model');
+
+const users = require('./routes/user'); 
+
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+    () => {console.log('Database is connected') },
+    err => { console.log('Can not connect to the database'+ err)}
+);
+
+app.use(methodOverride('X-HTTP-Method')) 
+app.use(methodOverride('X-HTTP-Method-Override'))
+app.use(methodOverride('X-Method-Override'))
+app.use(methodOverride('_method'))
+
+app.use(passport.initialize());
+require('./passport')(passport);
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/users', users);
+
+app.get('/', function(req, res) {
+    res.send('hello');
+});
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+});
